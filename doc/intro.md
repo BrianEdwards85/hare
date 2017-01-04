4042b238deda4b71e55dd514043e97fbd949fb7f
Brian Edwards <brian.edwards@yodle.com>
2017-01-04 13:33:20 -0600

    added seed files

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to hare
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/hare/core_test.clj
@@ -0,0 +1,7 @@
+(ns hare.core-test
+  (:require [clojure.test :refer :all]
+            [hare.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
