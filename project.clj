430fb9da6eec0c629fb88b3cfd211b455f3dad63
Brian Edwards <brian.edwards@yodle.com>
2017-01-04 13:30:23 -0600

    Update project.clj

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject hare "0.1.0-SNAPSHOT"
+  :description "Microservice RPC Library using RabbitMQ as transport"
+  :url "https://github.com/BrianEdwards85/hare"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.novemberain/langohr "3.7.0"]
+                 [clj-time "0.12.2"]])
